44cd3aaf65cd27d16e90c119fc499e8c55ae643c
Alexanderpinon <anderspin99@icloud.com>
2022-07-11 15:29:13 -0700

    update file

--- file_transfer.py
+++ file_transfer.py
@@ -3,33 +3,37 @@ from tkinter import *
 import tkinter.filedialog
 import os
 import shutil
-import schedule
-import time
+import datetime
+from datetime import timedelta
+
+
 
 class ParentWindow(Frame):
-    #Creates button to select files from source directory
-    self.sourceDir_btn = Button(text="Select Source", width=20)
-    #Positions source button in GUI using tkinter grid()
-    self.sourceDir_btn.grid(row=0, column=0, padx=(20, 10), pady=(30, 0))
-
-    #creates entry for source directory selection
-    self.source_dir = Entry(width=75)
-    #postions entry in GUI using tkinter grid() padx and pady are the same as
-    #the button to ensure they will line up
-    self.source_dir.grid(row=0, column=1, columnspan=2, padx=(20, 10), pady=(30,0))
-
-    self.destDir_btn = Button(text="Select Destination", width=20, command=self.sourceDir)
-    self.destDir_btn.grid(row=1, column=0, padx=(20, 10), pady=(15, 10))
-    self.destination_dir = Entry(width=75)
-    self.destination_dir.grid(row=1, column=1, columnspan=2, padx=(20, 10), pady=(15,10))
                                             
-    def _init_(self, master):
-        Frame._init_(self)
+    def __init__(self, master):
+        Frame.__init__(self)
         self.master.title("File Transfer")
         self.transfer_btn = Button(text="Transfer Files", width=20, command=self.transferFiles)
-        self.transfer_btn.grid(row=2, column=1, padx=(200,0), pady=(0, 15))
+        self.transfer_btn.grid(row=2, column=1, padx=(20,0), pady=(0, 15))
         self.exit_btn = Button(text="Exit", width=20, command=self.exit_program)
-        self.exit_btn.grid(row=20, column=2, padx=(10, 40), pady=(0, 15))
+        self.exit_btn.grid(row=2, column=2, padx=(10, 40), pady=(0, 15))
+        #Creates button to select files from source directory
+        self.sourceDir_btn = Button(text="Select Source", width=20, command=self.sourceDir)
+        #Positions source button in GUI using tkinter grid()
+        self.sourceDir_btn.grid(row=0, column=0, padx=(20, 10), pady=(30, 0))
+
+        #creates entry for source directory selection
+        self.source_dir = Entry(width=75)
+        #postions entry in GUI using tkinter grid() padx and pady are the same as
+        #the button to ensure they will line up
+        self.source_dir.grid(row=0, column=1, columnspan=2, padx=(20, 10), pady=(30,0,))
+
+        self.destDir_btn = Button(text="Select Destination", width=20, command=self.sourceDes)
+        self.destDir_btn.grid(row=1, column=0, padx=(20, 10), pady=(15, 10))
+        self.destination_dir = Entry(width=75)
+        self.destination_dir.grid(row=1, column=1, columnspan=2, padx=(20, 10), pady=(15,10))
+                                            
+
                            
 
     def sourceDir(self):
@@ -37,15 +41,27 @@ class ParentWindow(Frame):
         self.source_dir.delete(0, END)
         self.source_dir.insert(0, selectSourceDir)
 
+    def sourceDes(self):
+        selectSourceDir = tkinter.filedialog.askdirectory()
+        self.destination_dir.delete(0, END)
+        self.destination_dir.insert(0, selectSourceDir)
+
+    
+
 
     def transferFiles(self):
         source = self.source_dir.get()
         destination = self.destination_dir.get()
         source_files = os.listdir(source)
         for i in source_files:
-            shutil.move(source + '/' + i, destination)
-            print(i + ' was successfully transferred.')
-        schedule.every(24).hour.do(transferFiles)
+            transfer = os.path.join(source, i)
+            time = datetime.datetime.now() - timedelta(hours = 24)
+            txt = os.path.getmtime(transfer)
+            file = datetime.datetime.fromtimestamp(txt)
+            if time < file:
+                shutil.move(source + '/' + i, destination)
+                print(i + ' was successfully transferred.')
+         
             
     def exit_program(self):
         root.destroy()
@@ -53,6 +69,6 @@ class ParentWindow(Frame):
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
+    root = Tk()
     App = ParentWindow(root)
     root.mainloop()
