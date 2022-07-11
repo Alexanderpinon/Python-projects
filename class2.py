67a4b68f3f8b1c5e1ad9801f1dd6c651996dc728
Alexanderpinon <anderspin99@icloud.com>
2022-07-11 16:14:41 -0700

    update

--- class2.py
+++ class2.py
@@ -5,14 +5,14 @@ class User:
 
 
 
-def getLoginInfo(self):
-    entry_name = input("Enter your name here")
-    entry_email = input("Enter your email: ")
-    entry_password = input("Enter your password: ")
-    if (entry_email == self.email and entry_password == self.password):
-        print("Welcome back, {}".format(self.name))
-    else:
-        print("You are not authorized for this page.")
+     def getLoginInfo(self):
+         entry_name = input("Enter your name here")
+         entry_email = input("Enter your email: ")
+         entry_password = input("Enter your password: ")
+         if (entry_email == self.email and entry_password == self.password):
+             print("Welcome back, {}".format(self.name))
+         else:
+             print("You are not authorized for this page.")
 
 
 class Empolyee(User):
@@ -22,18 +22,27 @@ class Empolyee(User):
 
 
 
-def getLoginInfo(self):
-    entry_email = input("Enter your email: ")
-    entry_name = input("Enter your name: ")
-    entry_pin = input("Enter your pin: ")
-    if (entry_email == self.email and entry_pin == self.pin_number):
-        print("Welcome back, {}".format(self.name))
-    else:
-        print("The pin or email is incorrect")
+     def getLoginInfo(self):
+         entry_email = input("Enter your email: ")
+         entry_name = input("Enter your name: ")
+         entry_pin = input("Enter your pin: ")
+         if (entry_email == self.email and entry_pin == self.pin_number):
+             print("Welcome back, {}".format(self.name))
+         else:
+             print("The pin or email is incorrect")
 
 class Customer(User):
     mailing_address = ' '
     mailing_list = True
+
+
+     def getLoginInfo(self):
+         mailing_address = input("Enter your mailing address: ")
+         mailing_list = input("Enter your mailing list: ")
+         if (mailing_address == self.mailing_address and mailing_list == self.mailing_list):
+             print("your address is{}".format(self.address))
+         else:
+             print("The mailing address is wrong")
     
 
 
@@ -41,11 +50,6 @@ class Customer(User):
     customer.getLoginInfo()
 
     manager = Employee()
-    manager = getLoginInfo()
+    manager.getLoginInfo()
 
 
-def __init__(self, name, email, password, account):
-    self.name = name
-    self.email = email
-    self.password = password
-    self.account = account
