53797fbdfccfd4882d7e3fe9be83d833dd3ca5ff
Alexanderpinon <anderspin99@icloud.com>
2022-07-11 16:25:59 -0700

    updated

--- assi.py
+++ assi.py
@@ -5,20 +5,22 @@ conn = sqlite3.connect('assi.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS tbl_data( \
-            ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-            col_txt TEXT, \
-            col_docx TEXT, \
-            col_png TEXT, \
-            col_mpg TEXT, \
-            col_pdf TEXT, \
-            col_jpg TEXT  \
-            )")
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_data(ID INTEGER PRIMARY KEY AUTOINCREMENT,\
+            col_txt TEXT)")
     conn.commit()
 
 
 conn = sqlite3.connect('assi.db')
 
 
-filelist_tuple = ("Hello.txt", "World.txt")
+filelist = ('information.docx', 'Hello.txt', 'myImage.png'\
+            'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
+
+for x in filelist:
+    if x.endswith('.txt'):
+        with conn:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO tbl_data (col_txt) VALUES (?)", (x,))
+            print(x)
+conn.close()
 
