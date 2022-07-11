75be0277d9a76cbc6f271298de0dfe0886a6113d
Alexanderpinon <anderspin99@icloud.com>
2022-07-11 16:50:18 -0700

    class

--- class.py
+++ class.py
@@ -5,25 +5,11 @@ class User:
      account = 0
 
 class Empolyee(User):
+     Empolyee_pin = "enter pin"
+     employee_name = " enter name"
 
 class Customer(User):
-   
+  mail_address  = "enter your address"
+  mail_listing = "enter your mail listing"
 
 
-def login(self):
-    entry_email = input("Enter your email: ")
-    entry_password = input("Enter your password: ")
-    if (entry_email == self.email and entry_password == self.password):
-        print("Welcome back, {}".format(self.name))
-    else:
-        print("You are not authorized for this page.")
-
-    new_user = User()
-    new_user.login()
-
-
-def __init__(self, name, email, password, account):
-    self.name = name
-    self.email = email
-    self.password = password
-    self.account = account
