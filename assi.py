da363d30b911c2f13aee8abd8753401c61dbccca
Alexanderpinon <anderspin99@icloud.com>
2022-07-11 15:54:58 -0700

    updated

--- assi.py
+++ assi.py
@@ -15,33 +15,10 @@ with conn:
             col_jpg TEXT  \
             )")
     conn.commit()
-conn.close()
+
 
 conn = sqlite3.connect('assi.db')
 
-with conn:
-    cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_data(col_txt) VALUES (?)", \
-                ('Hello.txt','World.txt'))
-    cur.execute("INSERT INTO tbl_data(col_docx) VALUES (?)", \
-                ('information.docx'))
-    cur.execute("INSERT INTO tbl_data(col_png) VALUES (?)", \
-                ('myImage.png'))
-   cur.execute("INSERT INTO tbl_data(col_mpg) VALUES (?)", \
-                ('myMovie.mpg'))
-    cur.execute("INSERT INTO tbl_data(col_pdf) VALUES (?)", \
-                ('data.pdf'))
-    cur.execute("INSERT INTO tbl_data(col_jpg) VALUES (?)", \
-                ('myPhoto.jpg'))
 
-    conn.commit()
-conn.close()
+filelist_tuple = ("Hello.txt", "World.txt")
 
-conn = sqlite3.connect('assi.db')
-with conn:
-    cur = conn.cursor()
-    cur.execute("SELECT col_txt WHERE col_txt = '.txt'")
-    varData = cur.fetchall()
-    for item in varData:
-        msg = "Data: {}".format(item[0])
-    print(msg)
