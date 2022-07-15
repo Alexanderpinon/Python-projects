ee0a29f9494cd122ba65f9a02c89fd01ed19e2f2
Alexanderpinon <anderspin99@icloud.com>
2022-07-15 15:31:14 -0700

    Django

---
+++ DjangoUniversty/Classes/campus/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class UniversityCampus(models.Model):
+    campus_name = models.CharField(max_length=60, default="", blank=True, null=False)
+    state =  models.CharField(max_length=2,default="", blank=True, null=False)
+    campus_id = models.IntegerField(default="", blank=True, null=False)
+
+    object = models.Manager()
+
+
+class Meta:
+    verbose_name_plural = "UniversityCampus"
+
+
